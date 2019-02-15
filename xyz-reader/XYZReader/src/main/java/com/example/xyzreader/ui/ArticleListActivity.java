924bf538fdf95255b1b8020bf79374281d08ea13
Sanmati Shah <sanmatishah@avaya.com>
2019-02-15 23:08:26 +0530

    Fixes 1. Beginning with Android 9, the Apache HTTP client library is removed from the bootclasspath and is not available to apps by default. Add it to AndroidManifest.xml to continue using it 2. Call setSupportActionBar(toolbar) to setup the Toolbar as app bar 3. Remove redundant specification of android:textColorLink in fragment_article_detail.xml 4. Remove @dimen/fab_margin from values-sw600dp since it has the same value in the default file 5. Update the @dimen/fab_margin to 16dp as per Material Design Guidelines 6. Update the elevation of CardView (root element of article item) to 1dp

--- xyz-reader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ xyz-reader/XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -13,6 +13,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.text.format.DateUtils;
 import android.util.Log;
@@ -55,6 +56,10 @@ public class ArticleListActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
 
         mSwipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
         mRecyclerView = findViewById(R.id.recycler_view);
